4401d9f09d53a110d6474a9245b4fef554c12ead
Sahar Hagbi <noamsahar17@gmail.com>
2019-08-27 19:42:44 +0300

    first commit to save all

---
+++ src/common/MAGitResourceConstants.java
@@ -0,0 +1,8 @@
+package common;
+
+public class MAGitResourceConstants
+{
+    public static final String MAGIT_STAGE = "/repository/repositoryStage.fxml";
+    public static final String STARTING_STAGE = "/starting/startingStage.fxml";
+
+}
