01304f42b53e17e115903e86f7442026f5c80b30
Sahar Hagbi <noamsahar17@gmail.com>
2019-08-29 19:21:21 +0300

    Initial Frightening Design

---
+++ Controller/src/primaryController/PrimaryController.java
@@ -0,0 +1,16 @@
+package primaryController;
+
+import System.*;
+import main.MAGitController;
+
+public class PrimaryController
+{
+    private Engine m_Engine = new Engine();
+    private MAGitController m_MagitController = new MAGitController();
+
+    public void CreateNewRepository()
+    {
+
+        //m_Engine.CreateNewLocalRepository();
+    }
+}
--- src/common/MAGitResourceConstants.java
+++
@@ -1,8 +0,0 @@
-package common;
-
-public class MAGitResourceConstants
-{
-    public static final String MAGIT_STAGE = "/repository/repositoryStage.fxml";
-    public static final String STARTING_STAGE = "/starting/startingStage.fxml";
-
-}
